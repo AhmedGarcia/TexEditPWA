3637f24db6435f28711c79f9f49041ddcd598562
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-18 20:18:07 -0400

    Corrected the path to the manifest file and icons.Multiple Instances of precacheAndRoute Issue was solved by ensuring that precacheAndRoute(self.__WB_MANIFEST); is only included once in the service worker and that the service worker is only registered once.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -175,10 +175,98 @@
 // };
 
 
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+
+// module.exports = (env, argv) => {
+//   const isProduction = argv.mode === 'production';
+
+//   return {
+//     mode: isProduction ? 'production' : 'development',
+//     entry: {
+//       main: './client/src/js/index.js',
+//       install: './client/src/js/install.js',
+//     },
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//       publicPath: '/',
+//     },
+//     plugins: [
+      
+//       new HtmlWebpackPlugin({
+//         template: './client/index.html',
+//         title: 'J.A.T.E',
+//       }),
+//       isProduction &&
+//         new InjectManifest({
+//           swSrc: './client/src-sw.js',
+//           swDest: 'src-sw.js',
+//         }),
+//       new WebpackPwaManifest({
+//         name: 'Just Another Text Editor',
+//         short_name: 'JATE',
+//         description: 'Takes notes with JavaScript syntax highlighting!',
+//         background_color: '#225cca',
+//         theme_color: '#225cca',
+//         start_url: '/',
+//         publicPath: '/',
+//         fingerprints: false, // Disable hashing for the manifest file
+//         icons: [
+//           {
+//             src: path.resolve('client/src/images/logo.png'),
+//             sizes: [96, 128, 192, 256, 384, 512],
+//             destination: path.join('assets', 'icons'),
+//           },
+//         ],
+//       }),
+//     ].filter(Boolean),
+//     module: {
+//       rules: [
+//         {
+//           test: /\.css$/i,
+//           use: ['style-loader', 'css-loader'],
+//         },
+//         {
+//           test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//           type: 'asset/resource',
+//         },
+//         {
+//           test: /\.m?js$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: ['@babel/preset-env'],
+//               plugins: ['@babel/plugin-transform-runtime'],
+//             },
+//           },
+//         },
+//       ],
+//     },
+//     devServer: {
+//       static: {
+//         directory: path.join(__dirname, 'dist'),
+//       },
+//       compress: true,
+//       port: 8080,
+//       hot: false, // Ensure HMR is disabled if you don't need it
+//       historyApiFallback: true, // Important for SPA routing
+//       proxy: {
+//         '/api': 'http://localhost:3000',
+//       },
+//     },
+//   };
+// };
+
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require('path');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -193,9 +281,9 @@ module.exports = (env, argv) => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
       publicPath: '/',
+      clean: true,
     },
     plugins: [
-      
       new HtmlWebpackPlugin({
         template: './client/index.html',
         title: 'J.A.T.E',
@@ -221,6 +309,27 @@ module.exports = (env, argv) => {
             destination: path.join('assets', 'icons'),
           },
         ],
+      }),
+      new WebpackManifestPlugin({
+        fileName: 'manifest.webmanifest',
+        generate: (seed, files) => {
+          return {
+            name: 'Just Another Text Editor',
+            short_name: 'JATE',
+            description: 'A simple text editor PWA!',
+            icons: files
+              .filter((file) => file.name.endsWith('.png'))
+              .map((file) => ({
+                src: file.name,
+                sizes: `${file.size}x${file.size}`,
+                type: 'image/png',
+              })),
+            start_url: '/',
+            display: 'standalone',
+            background_color: '#ffffff',
+            theme_color: '#225cca',
+          };
+        },
       }),
     ].filter(Boolean),
     module: {
@@ -260,7 +369,6 @@ module.exports = (env, argv) => {
     },
   };
 };
-
 
 
 
